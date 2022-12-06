cbcf5cda40d1d60184543465ee7da9b8cad342d7
Arnaud PERALTA <a.peralta@proxiad.com>
2022-12-06 15:24:20 +0100

    day5

---
+++ src/day5/one.rs
@@ -0,0 +1,48 @@
+use std::{fs::File, io::{BufReader, BufRead}};
+
+pub fn please_work() {
+    let file = File::open("./src/day5/input").unwrap();
+    let buffer = BufReader::new(file).lines();
+    let mut stack_count = 0;
+    let mut stacks: Vec<Vec<char>> = vec![];
+    let mut now_instructions = false;
+    for line in buffer {
+        let content = line.unwrap();
+        if now_instructions {
+            let splits = content.split(" ").collect::<Vec<&str>>();
+            let number_of_move = splits[1].parse::<usize>().unwrap();
+            let from = splits[3].parse::<usize>().unwrap() - 1;
+            let to = splits[5].parse::<usize>().unwrap() - 1;
+
+            for _ in 0..number_of_move {
+                let value = stacks.get_mut(from).unwrap().pop().unwrap();
+                stacks.get_mut(to).unwrap().push(value);
+            }
+        }
+        if content.is_empty() {
+            now_instructions = true;
+        }
+        if stack_count == 0 {
+            stack_count = content.len() / 4 + 1;
+            for _ in 0..stack_count {
+                stacks.push(Vec::new());
+            }
+        }
+        let mut next_is_crate = false;
+        for (i, c) in content.chars().enumerate() {
+            if next_is_crate {
+                let actual_stack = i / 4;
+                stacks.get_mut(actual_stack).unwrap().insert(0, c);
+                next_is_crate = false;
+            }
+            if c == '[' {
+                next_is_crate = true;
+            }
+        }
+    }
+    for i in 0..stack_count {
+        let value = stacks.get_mut(i).unwrap().pop().unwrap();
+        print!("{}", value);
+    }
+    print!("\n");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use day4::{one::please_work, two::please_work_again};
+use day5::{one::please_work, two::please_work_again};
 
-mod day4;
+mod day5;
 
 fn main() {
     please_work();
