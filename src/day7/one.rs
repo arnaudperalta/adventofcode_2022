1a32a39476d4da020dd6b0ea8d70c3a28943aca8
Arnaud PERALTA <arnaud.peralta@gmail.com>
2022-12-08 23:00:21 +0100

    day7

--- src/day7/one.rs
+++ src/day7/one.rs
@@ -4,7 +4,7 @@ struct TreeNode {
     folder_name: String,
     folder_size: usize,
     parent: Option<Rc<RefCell<TreeNode>>>,
-    children: Vec<Rc<RefCell<TreeNode>>> // only folders are children
+    children: Vec<Rc<RefCell<TreeNode>>>
 }
 
 pub fn please_work() {
@@ -20,28 +20,58 @@ pub fn please_work() {
         if line.contains("cd /") {
             continue;
         }
-        if line.contains("cd ") {
+        else if line.contains("cd ..") {
+            let temp = actual_node.borrow_mut().parent.clone().unwrap();
+            actual_node = temp;
+        }
+        else if line.contains("cd ") {
             let folder_name = line.split("$ cd ").nth(1).unwrap();
+            let mut to_change = Rc::new(RefCell::new(TreeNode {
+                folder_name: "bug".to_string(),
+                folder_size: 0,
+                parent: None,
+                children: vec![]
+            }));
+
             for node in actual_node.borrow_mut().children.iter() {
                 if node.borrow_mut().folder_name == folder_name {
-                    actual_node = Rc::clone(node);
+                    to_change = node.clone();
                     break;
                 }
             }
+            actual_node = to_change;
         }
-        if line.contains("dir ") {
+        else if line.contains("dir ") {
             let new_child = Rc::new(RefCell::new(TreeNode {
                 folder_name: line.split("dir ").nth(1).unwrap().to_string(),
                 folder_size: 0,
-                parent: Some(actual_node),
+                parent: Some(actual_node.to_owned()),
                 children: vec![]
             }));
             actual_node.borrow_mut().children.push(new_child);
             continue;
         }
-        if !line.contains("$") {
+        else if !line.contains("$") {
             let file_size = line.split(" ").nth(0).unwrap().parse::<usize>().unwrap();
-            println!("{}", file_size);
+            actual_node.borrow_mut().folder_size += file_size;
+            let mut parent = actual_node.borrow().parent.clone();
+            while parent.clone().is_some() {
+                let to_change = parent.clone(); 
+                to_change.as_ref().unwrap().borrow_mut().folder_size += file_size;
+                parent = to_change.as_ref().unwrap().borrow_mut().parent.clone();
+            }
         }
+    }
+    let mut stack_sizes = vec![];
+    get_sizes(root, &mut stack_sizes);
+    println!("{}", stack_sizes.iter().sum::<usize>());
+}
+
+fn get_sizes(tree: Rc<RefCell<TreeNode>>, sizes: &mut Vec<usize>) {
+    if tree.borrow_mut().folder_size < 100000 {
+        sizes.push(tree.borrow().folder_size.clone());
+    }
+    for node in tree.borrow_mut().children.iter() {
+        get_sizes(node.to_owned(), sizes);
     }
 }
--- src/day7/two.rs
+++ src/day7/two.rs
@@ -1,33 +1,81 @@
-use std::{fs::File, io::{BufReader, BufRead}, collections::VecDeque};
+use std::{fs::read_to_string, rc::Rc, cell::RefCell};
+
+struct TreeNode {
+    folder_name: String,
+    folder_size: usize,
+    parent: Option<Rc<RefCell<TreeNode>>>,
+    children: Vec<Rc<RefCell<TreeNode>>>
+}
 
 pub fn please_work_again() {
-    let file = File::open("./src/day6/input").unwrap();
-    let mut buffer = VecDeque::<char>::new();
-    for line in BufReader::new(file).lines() {
-        let content = line.unwrap();
-        if content.is_empty() {
-            break;
+    let text = read_to_string("./src/day7/input").unwrap();
+    let root = Rc::new(RefCell::new(TreeNode {
+        folder_name: "/".to_string(),
+        folder_size: 0,
+        parent: None,
+        children: vec![]
+    }));
+    let mut actual_node = Rc::clone(&root);
+    for line in text.lines() {
+        if line.contains("cd /") {
+            continue;
         }
-        for (i, c) in content.chars().enumerate() {
-            if buffer.len() >= 13 {
-                if !(buffer.iter().any(|&i| i == c)) {
-                    let mut is_bad = false;
-                    for (i, element) in buffer.iter().enumerate() {
-                        for (j, second_element) in buffer.iter().enumerate() {
-                            if i != j && *element == *second_element {
-                                is_bad = true;
-                                break;
-                            }
-                        }
-                    }
-                    if !is_bad {
-                        println!("{}", i + 1);
-                        return;
-                    }
+        else if line.contains("cd ..") {
+            let temp = actual_node.borrow_mut().parent.clone().unwrap();
+            actual_node = temp;
+        }
+        else if line.contains("cd ") {
+            let folder_name = line.split("$ cd ").nth(1).unwrap();
+            let mut to_change = Rc::new(RefCell::new(TreeNode {
+                folder_name: "bug".to_string(),
+                folder_size: 0,
+                parent: None,
+                children: vec![]
+            }));
+
+            for node in actual_node.borrow_mut().children.iter() {
+                if node.borrow_mut().folder_name == folder_name {
+                    to_change = node.clone();
+                    break;
                 }
-                buffer.pop_back();
             }
-            buffer.push_front(c);
+            actual_node = to_change;
         }
+        else if line.contains("dir ") {
+            let new_child = Rc::new(RefCell::new(TreeNode {
+                folder_name: line.split("dir ").nth(1).unwrap().to_string(),
+                folder_size: 0,
+                parent: Some(actual_node.to_owned()),
+                children: vec![]
+            }));
+            actual_node.borrow_mut().children.push(new_child);
+            continue;
+        }
+        else if !line.contains("$") {
+            let file_size = line.split(" ").nth(0).unwrap().parse::<usize>().unwrap();
+            actual_node.borrow_mut().folder_size += file_size;
+            let mut parent = actual_node.borrow().parent.clone();
+            while parent.clone().is_some() {
+                let to_change = parent.clone(); 
+                to_change.as_ref().unwrap().borrow_mut().folder_size += file_size;
+                parent = to_change.as_ref().unwrap().borrow_mut().parent.clone();
+            }
+        }
+    }
+    let mut stack_sizes = vec![];
+    let base_size = root.borrow().folder_size;
+    get_sizes(root, &mut stack_sizes, base_size);
+    stack_sizes.sort();
+    println!("{}", stack_sizes.first().unwrap());
+}
+
+fn get_sizes(tree: Rc<RefCell<TreeNode>>, sizes: &mut Vec<usize>, base_size: usize) {
+    if 70000000 - base_size + tree.borrow_mut().folder_size > 30000000 {
+        let wesh = tree.borrow();
+        println!("name {}, size {}", wesh.folder_name, wesh.folder_size);
+        sizes.push(wesh.folder_size);
+    }
+    for node in tree.borrow_mut().children.iter() {
+        get_sizes(node.to_owned(), sizes, base_size);
     }
 }
